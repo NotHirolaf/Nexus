fe61eff11d53f5ffa8691ae7bb177c912f888304
NotHirolaf <venanciusfarrel18@gmail.com>
2026-01-25 16:40:13 -0500

    chore: Prepare for web deployment and added app icons

--- forge.config.cjs
+++ forge.config.cjs
@@ -4,6 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: './public/icon' // Electron automatically adds .ico for Windows, .icns for Mac
   },
   rebuildConfig: {},
   makers: [
